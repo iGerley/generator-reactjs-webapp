c60cd57ca9175eeb528f087e3a5e73dcc7d8c9d9
iGerley <gerleyadrianomiranda@gmail.com>
2018-04-11 21:51:18 -0300

    Setuped the the style loader.

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,12 @@ module.exports = {
                 loader: 'babel-loader',
                 test: /\.js$/,
                 exclude: /node_modules/
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader'
+                ]
             }
         ]
     },
